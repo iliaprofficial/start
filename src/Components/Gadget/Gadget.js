02727814bddce5f9efb3e8313cc9aa0a9a878c6b
Ilia Pro <iliapro@outlook.com>
2021-04-03 12:39:53 +0300

    Initial commit

---
+++ src/Components/Gadget/Gadget.js
@@ -0,0 +1,191 @@
+import './Gadget.css';
+
+const Gadget = ({gadget}) => {
+    return (
+        <div className={`Gadget ${gadget}`}>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/ddfd68ba-50a6-432a-bc6e-7de9d2166535/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemVertical ${gadget}`}>
+                    <div className={'GadgetItemVerticalPhone'}/>
+                </div>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    1
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("img/happy.png")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    2
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/4bb2ead2-7330-4f80-a643-929934f45ac4/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemVertical ${gadget}`}>
+                    <div className={'GadgetItemVerticalPhone'}/>
+                </div>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    3
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/50098509-501d-4211-bc32-0832cdb6023a/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    4
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/349bfabb-67ba-4f23-9c77-cdb48944e306/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemVertical ${gadget}`}>
+                    <div className={'GadgetItemVerticalPhone'}/>
+                </div>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    5
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/525133bd-d1dd-41aa-a796-c762af82bf4f/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemVertical ${gadget}`}>
+                    <div className={'GadgetItemVerticalPhone'}/>
+                </div>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    6
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("img/sadly.png")'}}
+                className={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    7
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/2193e538-8b61-412c-a31b-231bdc62b849/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemVertical ${gadget}`}>
+                    <div className={'GadgetItemVerticalPhone'}/>
+                </div>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    8
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/a01bc7ea-d18e-4f8e-b971-f098d6e27f83/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    9
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/ba79433c-84c0-4ce7-b4a4-bfcfe89ca3bf/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    10
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/d16d4b7a-f7a7-4be6-8049-c671d1ee0fc6/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    11
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    12
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/c340e00b-17ca-4ad2-b55a-a941a524926f/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    13
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/df82213b-be04-4720-85ac-0f5e62c48e4c/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    14
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/f358d953-6c80-4720-bf15-07a78ef6e094/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    15
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/c6f44cfc-c9ca-412c-b58a-3783b1a19e57/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    16
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/c5dc0f45-fbc9-4289-8e4d-2b39adaac944/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    17
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/02a9860f-bc9a-420b-9901-b418c22cf963/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    18
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/d0fb6056-566f-45ee-86ce-bf8937880659/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    19
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/d5803a5f-70e2-4725-851f-0d37b64c2a76/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    20
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/a586f65d-8046-4ffe-bce3-31d50b7301dd/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    21
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/f55f782b-2dcd-4e4c-be87-0cc13333f857/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    22
+                </div>
+            </div>
+            <div
+                style={{backgroundImage: 'url("https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/eed1de3a-5400-43b3-839e-22490389bf54/600x900")'}}
+                class={'GadgetItem'}>
+                <div className={`GadgetItemNumber ${gadget}`}>
+                    23
+                </div>
+            </div>
+            <div className={`GadgetListen ${gadget}`}>Я Вас слушаю</div>
+            <div className={`GadgetSearchPc ${gadget}`}>Поиск</div>
+            <div className={`GadgetSearchPhone ${gadget}`}>
+                <img src="https://img.icons8.com/windows/16/ffffff/search--v1.png"/>
+            </div>
+        </div>
+    );
+}
+
+export default Gadget;
